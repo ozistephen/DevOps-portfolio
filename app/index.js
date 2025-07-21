2d02194e55cbb3a03cd57a4399ba67bb6002024a
User <mac@Users-iMac.broadband>
2025-07-21 16:29:45 +0100

    Fix crash: remove undefined res.send call

--- app/index.js
+++ app/index.js
@@ -4,5 +4,3 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from DevOps Portfolio on ECS!'));
 app.listen(port, () => console.log(`App running on port ${port}`));
-
-res.send("Hello from updated ECS pipeline!");
